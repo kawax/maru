804570604b498502d78afd098f56ebe98f17eff8
kawax <kawaxbiz@gmail.com>
2020-11-27 15:25:44 +0900

    rm

--- config/fortify.php
+++ config/fortify.php
@@ -130,14 +130,14 @@ return [
     */
 
     'features' => [
-        //        Features::registration(),
-        //        Features::resetPasswords(),
-        // Features::emailVerification(),
-        //        Features::updateProfileInformation(),
-        //        Features::updatePasswords(),
-        //        Features::twoFactorAuthentication([
-        //            'confirmPassword' => true,
-        //        ]),
+        //                Features::registration(),
+        //                Features::resetPasswords(),
+        //                Features::emailVerification(),
+        //                Features::updateProfileInformation(),
+        //                Features::updatePasswords(),
+        //                Features::twoFactorAuthentication([
+        //                    'confirmPassword' => true,
+        //                ]),
     ],
 
 ];
